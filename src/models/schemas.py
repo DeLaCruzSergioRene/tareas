b093009abb42910754da279dd736437a000068d8
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2026-03-23 11:53:00 -0500

    listo

---
+++ src/models/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from datetime import date, time
+
+class UsuarioSchema(BaseModel):
+    nombre: str = Field(min_length=3, max_lenght=200)
+    email: EmailStr
+    password: str = Field(min_lenght=8)
+    
+class TareaSchema(BaseModel):
+    titulo: str = Field(min_lenght=1, max_lenght=200)
+    descripcion: Optional[str] = None
+    prioridad: str = "media"
+    clasificacion: str = "personal"
