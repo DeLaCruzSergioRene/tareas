4c35f5fb5ffdfb9a6c3ef807676667bf08fca8b5
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2026-04-13 09:53:26 -0500

    creo que ya

--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from tareas!")
+import flet as ft
+from src.controllers.UserController import AuthController
+from src.controllers.TareaController import TareaController
+from src.views.LoginView import LoginView
+from src.views.Dashboard import DashboardView
 
+def main(page: ft.Page):
+    # Instanciamos los controladores una sola vez
+    auth_ctrl = AuthController()
+    task_ctrl = TareaController()
 
-if __name__ == "__main__":
-    main()
+    def route_change(route):
+        page.views.clear()
+        if page.route == "/":
+            page.views.append(LoginView(page, auth_ctrl))
+        elif page.route == "/dashboard":
+            page.views.append(DashboardView(page, task_ctrl))
+        # Agregas aqui el registro_view de la misma forma
+        page.update()
+
+    page.on_route_change = route_change
+    page.go("/")
+
+if __name__ == "_main_":
+    ft.app(main)
