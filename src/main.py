c198fa04ca219177a5b6be59732960cf55c68c13
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2026-04-14 08:23:01 -0500

    a ver

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from controllers.TareaController import TareaController
 from views.LoginView import LoginView
 from views.Dashboard import DashboardView
 
-def main(page: ft.Page):
+def start(page: ft.Page):
     # Instanciamos los controladores una sola vez
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
@@ -18,8 +18,21 @@ def main(page: ft.Page):
         # Agregas aqui el registro_view de la misma forma
         page.update()
 
+    # Caso de seguridad: Si algo falla, mostrar texto de error
+    if not page.views:
+        page.views.append(
+            ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
+        )
+        
+    page.update()
+    
     page.on_route_change = route_change
+    # Forzamos la navegación inicial
     page.go("/")
-
-if __name__ == "_main_":
-    ft.app(main)
+    
+def main():
+    # Ejecución de la app
+    ft.app(target=start)
+    
+if __name__ == "__main__":
+    main()
