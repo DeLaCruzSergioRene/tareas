8be9648eac7b6ee7d5ee89a899ebeb619f59d0ac
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2026-04-23 12:15:11 -0500

    ok

--- models/schemasModel.py
+++ models/schemasModel.py
@@ -3,12 +3,12 @@ from typing import Optional
 from datetime import date, time
 
 class UsuarioSchema(BaseModel):
-    nombre: str = Field(min_length=3, max_lenght=100)
+    nombre: str = Field(min_length=3, max_length=100)
     email: EmailStr
     password: str = Field(min_lenght=8)
     
 class TareaSchema(BaseModel):
-    titulo: str = Field(min_lenght=1, max_lenght=200)
+    titulo: str = Field(min_lenght=1, max_length=200)
     descripcion: Optional[str] = None
     prioridad: str = "media"
     clasificacion: str = "personal"
